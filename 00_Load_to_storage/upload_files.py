67515b6fc1f22f1b55ccb98544fd252d2b7f6f09
matheusmafraoandrade <matheusmafrabh@gmail.com>
2024-06-26 23:50:44 -0300

    Upload files to GCS

---
+++ 00_Load_to_storage/upload_files.py
@@ -0,0 +1,88 @@
+#%% Packages
+import os
+from tqdm import tqdm
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.fs as fs
+import pyarrow.parquet as pq
+
+from dotenv import load_dotenv
+from google.cloud import storage
+
+load_dotenv()
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]  = os.getenv('GCP_CLOUD_STORAGE_SA')
+
+#%% Functions
+# define function that creates the bucket
+def create_bucket(bucket_name: str,
+                  storage_class: str = 'STANDARD',
+                  location: str = 'us-east1') -> str: 
+    
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(f"analytics_journey_{bucket_name}")
+    bucket.storage_class = storage_class
+   
+    bucket = storage_client.create_bucket(bucket, location=location)
+    
+    return print(f'Bucket {bucket.name} created.')
+
+# define function that uploads a file from the bucket
+def upload_files(bucket_name: str,
+                 partition_col: str = None) -> str:
+    
+    gcs = fs.GcsFileSystem()
+    
+    # Read the CSV file
+    path = fr"data\ERP\{bucket_name}.csv"
+    csv_data = pd.read_csv(path)
+    
+    if partition_col:
+        csv_data['reference_date'] = pd.to_datetime(csv_data[partition_col]).dt.date
+        uri = fr"analytics_journey_{bucket_name}/partitioned_data/"
+        
+        table = pa.Table.from_pandas(csv_data)
+        pq.write_to_dataset(
+            table,
+            root_path=uri,
+            filesystem=gcs,
+            partition_cols=['reference_date']
+        )
+    else:
+        uri = fr"analytics_journey_{bucket_name}/{bucket_name}.parquet"
+        
+        table = pa.Table.from_pandas(csv_data)
+        pq.write_table(
+            table,
+            uri,
+            filesystem=gcs
+        )
+    
+    return print(f"Parquet files {bucket_name} created!")
+        
+
+#%% Load
+files_dir = os.listdir(r'data\ERP')
+
+for file in tqdm(files_dir, total=len(files_dir), desc="GCS upload progress"):
+    bucket_name = file.split('.')[0]
+    
+    match bucket_name:
+        case 'order_items':
+            partition_col = 'shipping_limit_date'
+        case 'order_reviews':
+            partition_col = 'review_creation_date'
+        case 'orders':
+            partition_col = 'order_purchase_timestamp'
+        case _:
+            partition_col = None
+
+    create_bucket(bucket_name)
+    upload_files(bucket_name, partition_col)
+    
+    print("--------------------------")
+
+print("Uploaded files successfully!")
+
+# %%
