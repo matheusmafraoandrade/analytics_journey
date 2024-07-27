488680214ed3b33eba8f3f83ea37c2762649163c
matheusmafraoandrade <matheusmafrabh@gmail.com>
2024-07-27 13:22:34 -0300

    Documented code

--- 00_Load_to_storage/upload_files.py
+++ 00_Load_to_storage/upload_files.py
@@ -1,20 +1,26 @@
-#%% Packages
-import os
+#%%
+# Monitor upload progress 
 from tqdm import tqdm
 
+# Deal with dataframes and formats
 import pandas as pd
 import pyarrow as pa
 import pyarrow.fs as fs
 import pyarrow.parquet as pq
 
-from dotenv import load_dotenv
+# Connect to Google Cloud Storage
 from google.cloud import storage
 
+# Configure environment
+import os
+from dotenv import load_dotenv
+
+# It is important to name the credentials as GOOGLE_APPLICATION_CREDENTIALS for the Storage package to work accordingly 
 load_dotenv()
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]  = os.getenv('GCP_CLOUD_STORAGE_SA')
 
-#%% Functions
-# define function that creates the bucket
+#%%
+# Function that creates the bucket
 def create_bucket(bucket_name: str,
                   storage_class: str = 'STANDARD',
                   location: str = 'us-east1') -> str: 
@@ -28,21 +34,25 @@ def create_bucket(bucket_name: str,
     
     return print(f'Bucket {bucket.name} created.')
 
-# define function that uploads a file from the bucket
+# Function that uploads a file to the bucket
 def upload_files(bucket_name: str,
                  partition_col: str = None) -> str:
     
     # Read the CSV file
     path = fr"data\ERP\{bucket_name}.csv"
     csv_data = pd.read_csv(path)
+    
+    # Convert pandas dataframe to pyarrow table
     table = pa.Table.from_pandas(csv_data)
     
-    # Cloud Storage file system
+    # Instantiate Cloud Storage file system
     gcs = fs.GcsFileSystem()
     
+    # Write Arrow table to Parquet files
     if partition_col:
         uri = fr"analytics_journey_{bucket_name}/partitioned_data/"
         
+        # Files partitioned by state
         if "state" in partition_col:
             pq.write_to_dataset(
                 table,
@@ -50,6 +60,7 @@ def upload_files(bucket_name: str,
                 filesystem=gcs,
                 partition_cols=[partition_col]
             )
+        # Files partitioned by date
         else:
             csv_data['reference_date'] = pd.to_datetime(csv_data[partition_col]).dt.date
             pq.write_to_dataset(
@@ -58,6 +69,7 @@ def upload_files(bucket_name: str,
                 filesystem=gcs,
                 partition_cols=['reference_date']
             )
+    # Files without partition
     else:
         uri = fr"analytics_journey_{bucket_name}/{bucket_name}.parquet"
         
@@ -70,12 +82,14 @@ def upload_files(bucket_name: str,
     return print(f"Parquet files {bucket_name} created!")
         
 
-#%% Load
+#%%
 files_dir = os.listdir(r'data\ERP')
 
+# Create a bucket for each file and load converted Parquet files
 for file in tqdm(files_dir, total=len(files_dir), desc="GCS upload progress"):
     bucket_name = file.split('.')[0]
     
+    # Define the partition column for each file
     match bucket_name:
         case 'geolocation':
             partition_col = 'geolocation_state'
@@ -86,7 +100,7 @@ for file in tqdm(files_dir, total=len(files_dir), desc="GCS upload progress"):
         case 'orders':
             partition_col = 'order_purchase_timestamp'
         case _:
-            partition_col = None
+            partition_col = None # No partition for other files
 
     create_bucket(bucket_name)
     upload_files(bucket_name, partition_col)
